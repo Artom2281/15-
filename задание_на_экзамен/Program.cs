7cdfb1dee9c3059714f6748f9b8fede97881c4c5
Artom2281 <artems82213@gmail.com>
2025-12-05 09:06:43 +0300

    лгещшгнанглнг

---
+++ задание_на_экзамен/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+public class Program
+{
+    static void Main()
+    {
+        int[] m = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        number j = new number();
+        Console.WriteLine(number.Mini(m));
+
+      
+    }
+    [Fact]
+    public void test()
+    {
+        int[] m = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        number j = new number();
+        Assert.Equal(number.Mini(m),1);
+    }
+}
+class number
+{
+    public static int Mini(int[] n) => n.Min();
+ 
+}
+       
